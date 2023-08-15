8ef2dbf50f731c620ebe35d02b583ca0d17e5274
H-Shaikh <72442248+H-Shaikh@users.noreply.github.com>
2023-08-15 19:30:33 +0530

    some fixes

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from store.models import Product, Customer, Collection, Order, OrderItem
 
-# Create your views here.
-def say_hello(request):
-    # return HttpResponse('Hello World!')
-    return render(request, 'hello.html',{'name': 'hamza'})
+
+def say_hello(request):  
+    return render(request, 'hello.html',{'name': 'Satoru'})
