67a55a1cc5b4154cab30be8f6381d33994949843
H-Shaikh <72442248+H-Shaikh@users.noreply.github.com>
2023-08-16 22:51:36 +0530

    new changes in admin

--- store/models.py
+++ store/models.py
@@ -10,6 +10,12 @@ class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')#related_name='+' tells django not to create relationship
     
+    def __str__(self) -> str:
+        return self.title    
+    
+    class Meta:
+        ordering = ['title']
+    
 class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
@@ -19,6 +25,13 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now_add=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotion = models.ManyToManyField(Promotion)
+    
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
+    
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
@@ -37,6 +50,13 @@ class Customer(models.Model):
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+    
+    class Meta:
+        ordering = ['first_name', 'last_name']
+    
     
 class Order(models.Model):
     
