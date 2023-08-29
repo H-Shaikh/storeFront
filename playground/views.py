f88349401fe3ddfe168b430ce4063f4e1d32ff74
H-Shaikh <72442248+H-Shaikh@users.noreply.github.com>
2023-08-29 15:08:01 +0530

    played with fake emails

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
+from templated_mail.mail import BaseEmailMessage
 
 
-def say_hello(request):  
-    return render(request, 'hello.html',{'name': 'Satoru'})
+def say_hello(request): 
+    try:
+        message = BaseEmailMessage(
+            template_name='emails/hello.html',
+            context={'name':'Satoru'} 
+        )
+        message.send(['john@satorubuy.com'])
+    except BadHeaderError:
+        pass
+    return render(request, 'hello.html', {'name': 'Satoru'})
